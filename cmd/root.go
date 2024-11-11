20108f89016e380da7a0d63853aecaf223907690
Wallan <wallanaq.dev@gmail.com>
2024-11-11 10:24:14 -0300

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var Verbose bool
+var Version string = "v0.1.0"
+
+var RootCmd = &cobra.Command{
+	Use:   "oidc",
+	Short: "CLI for OIDC authentication",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if Verbose {
+			log.SetOutput(os.Stdout)
+		} else {
+			log.SetOutput(io.Discard)
+		}
+	},
+}
+
+func init() {
+	RootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+}
